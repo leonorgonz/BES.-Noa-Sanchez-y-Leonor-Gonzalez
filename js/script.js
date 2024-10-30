e2b0c10acceb16123ac50182066dd449164bcf85
leonorgonz <lellogonz@gmail.com>
2024-10-31 00:18:55 +0100

    orden y arreglo

--- js/script.js
+++ js/script.js
@@ -1,181 +1,133 @@
 const html = document.querySelector("html");
-html.setAttribute("data-bs-theme", "dark");
 
 // Data aos
 AOS.init();
-// BOTON DE ALERTA DE ARRIBA
 
 
+// MENU BOTÓN
 const menuButton = document.getElementById('menuButton');
         const bar = document.getElementById('bar');
         const overlay = document.getElementById('overlay');
         let menuVisible = false; // Estado del menú (oculto por defecto)
 
-        // Al hacer clic en el botón de la imagen
+        // CLICK
         menuButton.addEventListener('click', function () {
             if (!menuVisible) {
-                bar.style.width = '200px'; // Mueve la barra a su posición visible
-                menuButton.style.transform = 'translateX(200px)'; // Mueve el botón hacia la derecha
-                overlay.style.opacity = '1'; // Hace visible la capa de fondo
-                overlay.style.visibility = 'visible'; // Hace visible la capa de fondo
-                document.body.style.overflow = 'hidden'; // Desactiva el scroll
+                bar.style.width = '200px';
+                menuButton.style.transform = 'translateX(200px)';
+                overlay.style.opacity = '1';
+                overlay.style.visibility = 'visible';
+                document.body.style.overflow = 'hidden';
                 menuVisible = true;
             } else {
-                bar.style.width = '0'; // Regresa la barra a su posición oculta
-                menuButton.style.transform = 'translateX(0)'; // Regresa el botón también
-                overlay.style.opacity = '0'; // Oculta la capa de fondo
-                overlay.style.visibility = 'hidden'; // Oculta la capa de fondo
-                document.body.style.overflow = 'auto'; // Activa el scroll
+                bar.style.width = '0'; 
+                menuButton.style.transform = 'translateX(0)';
+                overlay.style.opacity = '0';
+                overlay.style.visibility = 'hidden';
+                document.body.style.overflow = 'auto';
                 menuVisible = false;
             }
         });
 
-        // Función para agregar la clase de vibración
+        // VIBRACIÓN
         function vibrateButton() {
-            if (!menuVisible) { // Solo vibrar si el menú está oculto
+            if (!menuVisible) { 
                 menuButton.classList.add('vibrate');
-                // Después de 300ms, removemos la clase para que se pueda volver a agregar
                 setTimeout(() => {
                     menuButton.classList.remove('vibrate');
                 }, 300);
             }
         }
-
-        // Hacer que la imagen vibre cada 3 segundos
         setInterval(vibrateButton, 2000);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// Variables para controlar la dirección
-let direction = 'next'; // 'next' para avanzar a la derecha, 'prev' para retroceder
-const carousel = document.getElementById('carouselExampleSlidesOnly');
-const bsCarousel = new bootstrap.Carousel(carousel, {
-  interval: false, // Desactivamos el ciclo automático
-  wrap: true // Mantener el loop continuo
-});
-
-// Función para cambiar de slide
-function changeSlide() {
-  const activeItem = document.querySelector('.carousel-item.active');
-  const totalItems = document.querySelectorAll('.carousel-item').length;
-  const activeIndex = [...activeItem.parentElement.children].indexOf(activeItem);
-
-  // Cambiar de dirección cuando llegue al final o al principio
-  if (activeIndex === totalItems - 1 && direction === 'next') {
-    direction = 'prev'; // Cambiar la dirección a "prev" (de derecha a izquierda)
-  } else if (activeIndex === 0 && direction === 'prev') {
-    direction = 'next'; // Cambiar la dirección a "next" (de izquierda a derecha)
-  }
-
-  // Cambiar el slide según la dirección
-  if (direction === 'next') {
-    bsCarousel.next();
-  } else {
-    bsCarousel.prev();
-  }
-
-  // Esperar 1 segundo antes de cambiar nuevamente
-  setTimeout(changeSlide, 1000); // 1000 ms = 1 segundo
-}
-
-// Iniciar el cambio de diapositivas
-setTimeout(changeSlide, 1000); // Iniciar después de 1 segundo
-
-
-// Evitar que el usuario haga scroll inicialmente
+// BLOQUEA SCROLL
 document.body.style.overflow = 'hidden';
-
-// Detectar cuando se hace clic en la flecha
 document.querySelector('.arrow-container').addEventListener('click', function() {
-  // Permitir el scroll
+  //DEJAR HACER SCROLL DE NUEVO TRANS DAR A NUEVA COLLECTION
   document.body.style.overflow = 'auto';
-
-  // Hacer scroll suave al wrapper
   document.querySelector('.wrapper').scrollIntoView({ behavior: 'smooth' });
 });
 
 
 
-    const carousels = [
-        '#galleryCarousel1',
-        '#galleryCarousel2',
-        '#galleryCarousel3'
-    ];
-
-    // Inicia el carrusel de Bootstrap para cada uno
-    carousels.forEach(carousel => {
-        const bsCarousel = new bootstrap.Carousel(document.querySelector(carousel), {
-            interval: 3000,
-            wrap: true
-        });
-    });
-
-    // Sincroniza los carruseles
-    setInterval(() => {
-        carousels.forEach(carousel => {
-            const bsCarousel = bootstrap.Carousel.getInstance(document.querySelector(carousel));
-            bsCarousel.next(); // Cambia a la siguiente imagen
-        });
-    }, 1000); // Cambia cada 3 segundos
+// CAROUSEL INICIO
+$(document).ready(function () {
+    let currentIndex = 0;
+    const items = $('#iniciocarouselInner .carousel-item');
+    const itemCount = items.length;
+    // CAMBIA IMAGEN
+    function changeImage() {
+        items.eq(currentIndex).removeClass('active');
+        currentIndex = (currentIndex + 1) % itemCount;
+        items.eq(currentIndex).addClass('active'); 
+    }
+    setInterval(changeImage, 1500);
+});
 
 
 
+// CAROUSEL 3 MODELOS
+$(document).ready(function () {
+    // Función para configurar el carrusel
+    function setupCarousel(carouselId) {
+        let currentIndex = 0; // Índice de la imagen actual
+        const items = $(`${carouselId} .carousel-item`); // Seleccionar los items del carrusel
+        const itemCount = items.length; // Número total de items
 
-    document.addEventListener('DOMContentLoaded', () => {
+        // Ocultar todos los items y mostrar solo el primero
+        items.hide(); // Ocultar todas las imágenes
+        items.eq(currentIndex).show(); // Mostrar la primera imagen
 
-      //===
-      // VARIABLES
-      //===
-      const DATE_TARGET = new Date('2/1/2025 5:01 AM');
-      // DOM for render
-      const SPAN_DAYS = document.querySelector('span#days');
-      const SPAN_HOURS = document.querySelector('span#hours');
-      const SPAN_MINUTES = document.querySelector('span#minutes');
-      const SPAN_SECONDS = document.querySelector('span#seconds');
-      // Milliseconds for the calculations
-      const MILLISECONDS_OF_A_SECOND = 1000;
-      const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
-      const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
-      const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
+        // Cambiar la imagen
+        setInterval(function () {
+            items.eq(currentIndex).hide(); // Ocultar imagen actual
+            currentIndex = (currentIndex + 1) % itemCount; // Avanzar al siguiente índice (volviendo a 0 si se llega al final)
+            items.eq(currentIndex).show(); // Mostrar la nueva imagen
+        }, 1000); // Cambiar cada 1 segundo
+    }
 
-      //===
-      // FUNCTIONS
-      //===
+    // Inicializar cada carrusel
+    setupCarousel('#galleryCarousel1');
+    setupCarousel('#galleryCarousel2');
+    setupCarousel('#galleryCarousel3');
+});
 
-      /**
-      * Method that updates the countdown and the sample
-      */
-      function updateCountdown() {
+
+
+
+// CUENTA ATRÁS
+document.addEventListener('DOMContentLoaded', () => {
+    const DATE_TARGET = new Date('2/1/2025 5:01 AM');
+    // DOM for render
+    const SPAN_DAYS = document.querySelector('span#days');
+    const SPAN_HOURS = document.querySelector('span#hours');
+    const SPAN_MINUTES = document.querySelector('span#minutes');
+    const SPAN_SECONDS = document.querySelector('span#seconds');
+    // Milliseconds for the calculations
+    const MILLISECONDS_OF_A_SECOND = 1000;
+    const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+    const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+    const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
+
+    function updateCountdown() {
           // Calcs
-          const NOW = new Date();
-          const DURATION = DATE_TARGET - NOW;
-          const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
-          const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
-          const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
-          const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
+        const NOW = new Date();
+        const DURATION = DATE_TARGET - NOW;
+        const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
+        const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
+        const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
+        const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
         
-          // Render
-          SPAN_DAYS.textContent = REMAINING_DAYS;
-          SPAN_HOURS.textContent = REMAINING_HOURS;
-          SPAN_MINUTES.textContent = REMAINING_MINUTES;
-          SPAN_SECONDS.textContent = REMAINING_SECONDS;
-      }
+        // Render
+        SPAN_DAYS.textContent = REMAINING_DAYS;
+        SPAN_HOURS.textContent = REMAINING_HOURS;
+        SPAN_MINUTES.textContent = REMAINING_MINUTES;
+        SPAN_SECONDS.textContent = REMAINING_SECONDS;
+    }
 
-      //===
-      // INIT
-      //===
-      updateCountdown();
-      // Refresh every second
-      setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
-    });
+    updateCountdown();
+    // Refresh every second
+    setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
+});
